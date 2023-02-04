6ade0b2aa27af62e8dcdca578ffb85062a537446
Shide Foo <shide.foo@gojek.com>
2023-02-05 00:08:18 +0800

    Add vim-helm syntax

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -8,15 +8,6 @@ local M = {
 			highlight = { enable = true },
 			auto_install = true,
 		})
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.gotmpl = {
-			install_info = {
-				url = "https://github.com/ngalaiko/tree-sitter-go-template",
-				files = { "src/parser.c" },
-			},
-			filetype = "gotmpl",
-			used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl" },
-		}
 	end,
 }
 return M
