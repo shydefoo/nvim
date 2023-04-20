db0a370ac8359921ee0b58d71f34b4f45a5b488c
shydefoo <shidefoo@gmail.com>
2023-04-21 01:00:12 +0800

    Clean up nvim-tree

--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -103,8 +103,6 @@ local M = {
     config = function()
         require("nvim-tree").setup({
             on_attach = on_attach,
-            open_on_setup = false,
-            open_on_setup_file = false,
             sort_by = "case_sensitive",
             hijack_netrw = false,
             disable_netrw = false,
@@ -120,23 +118,6 @@ local M = {
                     },
                 },
                 side = "right",
-                -- mappings = {
-                -- 	list = {
-                -- 		{ key = "u", action = "dir_up" },
-                -- 		{ key = "s", action = "vsplit" },
-                -- 		{ key = "v", action = "split" },
-                -- 		{ key = "i", action = "split" },
-                -- 		{ key = "t", action = "tabnew" },
-                -- 		{ key = "ma", action = "create" },
-                -- 		{ key = "md", action = "remove" },
-                -- 		{ key = "mD", action = "trash" },
-                -- 		{ key = "mm", action = "full_rename" },
-                -- 		{ key = "mr", action = "system_open" },
-                -- 		{ key = "C", action = "cd" },
-                -- 		{ key = "I", action = "toggle_dotfiles" },
-                -- 		{ key = "?", action = "toggle_help" },
-                -- 	},
-                -- },
             },
             renderer = {
                 icons = {
@@ -153,56 +134,6 @@ local M = {
             git = { ignore = false },
         })
     end,
-    -- opts = {
-    -- 	open_on_setup = false,
-    -- 	open_on_setup_file = false,
-    -- 	sort_by = "case_sensitive",
-    -- 	hijack_netrw = false,
-    -- 	disable_netrw = false,
-    -- 	view = {
-    -- 		float = {
-    -- 			enable = true,
-    -- 			open_win_config = {
-    -- 				relative = "editor",
-    -- 				width = 90,
-    -- 				height = 60,
-    -- 				row = (vim.api.nvim_list_uis()[1].height - 60) * 0.4,
-    -- 				col = (vim.api.nvim_list_uis()[1].width - 90) * 0.5,
-    -- 			},
-    -- 		},
-    -- 		side = "right",
-    -- 		-- mappings = {
-    -- 		-- 	list = {
-    -- 		-- 		{ key = "u", action = "dir_up" },
-    -- 		-- 		{ key = "s", action = "vsplit" },
-    -- 		-- 		{ key = "v", action = "split" },
-    -- 		-- 		{ key = "i", action = "split" },
-    -- 		-- 		{ key = "t", action = "tabnew" },
-    -- 		-- 		{ key = "ma", action = "create" },
-    -- 		-- 		{ key = "md", action = "remove" },
-    -- 		-- 		{ key = "mD", action = "trash" },
-    -- 		-- 		{ key = "mm", action = "full_rename" },
-    -- 		-- 		{ key = "mr", action = "system_open" },
-    -- 		-- 		{ key = "C", action = "cd" },
-    -- 		-- 		{ key = "I", action = "toggle_dotfiles" },
-    -- 		-- 		{ key = "?", action = "toggle_help" },
-    -- 		-- 	},
-    -- 		-- },
-    -- 	},
-    -- 	renderer = {
-    -- 		icons = {
-    -- 			git_placement = "after",
-    -- 			glyphs = {
-    -- 				git = {
-    -- 					untracked = "u",
-    -- 				},
-    -- 			},
-    -- 		},
-    -- 		group_empty = true,
-    -- 	},
-    -- 	filters = { dotfiles = true },
-    -- 	git = { ignore = false },
-    -- },
 }
 
 return M
