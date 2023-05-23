939c6cfab655e9f99432ec4d98813ef4a7f90d58
Shide Foo <shide.foo@gojek.com>
2023-05-23 13:32:50 +0800

    Temp commit

--- lua/core/plugins/ale.lua
+++ lua/core/plugins/ale.lua
@@ -12,23 +12,23 @@ local M = {
 			terraform = { "terraform" },
 			ruby = { "prettier", "rubocop", "rufo" },
 		}
-		vim.g.ale_linters = {
-			javascript = { "eslint" },
-			python = { "flake8" },
-			go = { "golangci-lint" },
-			sh = { "language_server", "shellcheck" },
-			lua = { "luac" },
-			terraform = { "tflint", "terraform_ls", "terraform" },
-			ruby = { "rubocop" },
-		}
+		-- vim.g.ale_linters = {
+		-- javascript = { "eslint" },
+		-- python = { "flake8" },
+		-- go = { "golangci-lint" },
+		-- sh = { "language_server", "shellcheck" },
+		-- lua = { "luac" },
+		-- terraform = { "tflint", "terraform_ls", "terraform" },
+		-- ruby = { "rubocop" },
+		-- }
 		vim.g.ale_fix_on_save = 1
 		vim.g.ale_lint_on_save = 0
-		vim.g.ale_lint_on_insert_leave = 1
-		vim.g["airline#extensions#ale#enabled"] = 1
+		vim.g.ale_lint_on_insert_leave = 0
+		vim.g["airline#extensions#ale#enabled"] = 0
 		vim.g.ale_sign_error = "**"
 		vim.g.ale_sign_warning = "*-"
-		vim.g.ale_linters_explicit = 1
-		vim.g.ale_set_signs = 1
+		vim.g.ale_linters_explicit = 0
+		vim.g.ale_set_signs = 0
 		vim.g.ale_python_flake8_options = "--max-line-length=120"
 		vim.g.ale_python_flake8_executable = "/Users/shide.foo/.pyenv/versions/3.7.8/bin/flake8"
 		-- vim.g.ale_pattern_options = {
--- lua/core/plugins/vim-go.lua
+++ lua/core/plugins/vim-go.lua
@@ -6,6 +6,7 @@ local M = {
 		vim.g.go_rename_command = "gopls"
 		vim.g.go_code_completion_enabled = 0
 		vim.g.go_doc_balloon = 1
+		vim.g.go_fmt_command = "gofmt"
 	end,
 }
 
