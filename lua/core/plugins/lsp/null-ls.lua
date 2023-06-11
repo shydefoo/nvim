ec722bb108c963a0636315ca13445d8488ffd5af
Shide Foo <shide.foo@gojek.com>
2023-06-11 12:11:25 +0800

    Modify golangci-lint

--- lua/core/plugins/lsp/null-ls.lua
+++ lua/core/plugins/lsp/null-ls.lua
@@ -2,48 +2,52 @@ local nls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 nls.setup({
-	sources = {
-		nls.builtins.formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", "2" } }),
-		nls.builtins.diagnostics.flake8.with({
-			extra_args = { "--max-line-length=120" },
-		}),
-		nls.builtins.diagnostics.eslint_d,
-		-- nls.builtins.diagnostics.golangci_lint,
-		nls.builtins.formatting.prettier.with({
-			extra_args = { "--single-quote", "false" },
-		}),
-		nls.builtins.formatting.terraform_fmt,
-		nls.builtins.formatting.black,
-		nls.builtins.formatting.goimports,
-		nls.builtins.formatting.gofmt,
-		-- nls.builtins.formatting.gofumpt,
-		nls.builtins.formatting.latexindent.with({
-			extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
-		}),
-		nls.builtins.code_actions.shellcheck,
-		nls.builtins.code_actions.gitsigns,
-		nls.builtins.formatting.shfmt,
-		-- nls.builtins.diagnostics.ruff,
-	},
-	on_attach = function(client, bufnr)
-		vim.keymap.set(
-			"n",
-			"<leader>fx",
-			"<cmd>lua require('core.plugins.lsp.utils').toggle_autoformat()<cr>",
-			{ desc = "Toggle format on save" }
-		)
-		-- NOTE: not sure what this does, so comment out first
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					if AUTOFORMAT_ACTIVE then -- global var defined in functions.lua
-						vim.lsp.buf.format({ bufnr = bufnr })
-					end
-				end,
-			})
-		end
-	end,
+    debug = true,
+    sources = {
+        nls.builtins.formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", "2" } }),
+        nls.builtins.diagnostics.flake8.with({
+            extra_args = { "--max-line-length=120" },
+        }),
+        nls.builtins.diagnostics.eslint_d,
+        nls.builtins.formatting.prettier.with({
+            extra_args = { "--single-quote", "false" },
+        }),
+        nls.builtins.formatting.terraform_fmt,
+        nls.builtins.formatting.black.with({
+            command = "/Users/shide.foo/.pyenv/versions/3.7.8/bin/black",
+        }),
+        nls.builtins.formatting.goimports,
+        nls.builtins.formatting.gofmt,
+        nls.builtins.diagnostics.golangci_lint.with({
+            timeout = 10000,
+        }),
+        nls.builtins.formatting.latexindent.with({
+            extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
+        }),
+        nls.builtins.code_actions.shellcheck,
+        nls.builtins.code_actions.gitsigns,
+        nls.builtins.formatting.shfmt,
+        -- nls.builtins.diagnostics.ruff,
+    },
+    on_attach = function(client, bufnr)
+        vim.keymap.set(
+            "n",
+            "<leader>fx",
+            "<cmd>lua require('core.plugins.lsp.utils').toggle_autoformat()<cr>",
+            { desc = "Toggle format on save" }
+        )
+        -- NOTE: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    if AUTOFORMAT_ACTIVE then -- global var defined in functions.lua
+                        vim.lsp.buf.format({ bufnr = bufnr })
+                    end
+                end,
+            })
+        end
+    end,
 })
--- lua/core/plugins/lsp/utils.lua
+++ lua/core/plugins/lsp/utils.lua
@@ -6,13 +6,13 @@ M = {}
 VIRTUAL_TEXT_ACTIVE = true
 -- toggle displaying virtual text
 M.toggle_virtual_text = function()
-	VIRTUAL_TEXT_ACTIVE = not VIRTUAL_TEXT_ACTIVE
-	utils.notify(
-		string.format("Virtualtext %s", VIRTUAL_TEXT_ACTIVE and "on" or "off"),
-		vim.log.levels.INFO,
-		"lsp/utils.lua"
-	)
-	vim.diagnostic.show(nil, 0, nil, { virtual_text = VIRTUAL_TEXT_ACTIVE })
+    VIRTUAL_TEXT_ACTIVE = not VIRTUAL_TEXT_ACTIVE
+    utils.notify(
+        string.format("Virtualtext %s", VIRTUAL_TEXT_ACTIVE and "on" or "off"),
+        vim.log.levels.INFO,
+        "lsp/utils.lua"
+    )
+    vim.diagnostic.show(nil, 0, nil, { virtual_text = VIRTUAL_TEXT_ACTIVE })
 end
 
 -- TODO: refactor
@@ -20,33 +20,33 @@ end
 AUTOFORMAT_ACTIVE = true
 -- toggle null-ls's autoformatting
 M.toggle_autoformat = function()
-	AUTOFORMAT_ACTIVE = not AUTOFORMAT_ACTIVE
-	utils.notify(
-		string.format("Autoformatting %s", AUTOFORMAT_ACTIVE and "on" or "off"),
-		vim.log.levels.INFO,
-		"lsp.utils"
-	)
+    AUTOFORMAT_ACTIVE = not AUTOFORMAT_ACTIVE
+    utils.notify(
+        string.format("Autoformatting %s", AUTOFORMAT_ACTIVE and "on" or "off"),
+        vim.log.levels.INFO,
+        "lsp.utils"
+    )
 end
 
 -- detect python venv
 -- https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-851247107
 M.get_python_path = function(workspace)
-	local lsp_util = require("lspconfig/util")
-	local path = lsp_util.path
-	-- Use activated virtualenv.
-	if vim.env.VIRTUAL_ENV then
-		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
-	end
-	-- Find and use virtualenv in workspace directory.
-	-- NOTE: Figure out what this does (don't use workspaces)
-	-- for _, pattern in ipairs({ "*", ".*" }) do
-	-- 	local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
-	-- 	if match ~= "" then
-	-- 		return path.join(path.dirname(match), "bin", "python")
-	-- 	end
-	-- end
-	-- Fallback to system Python.
-	return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+    local lsp_util = require("lspconfig/util")
+    local path = lsp_util.path
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+    end
+    -- Find and use virtualenv in workspace directory.
+    -- NOTE: Figure out what this does (don't use workspaces)
+    -- for _, pattern in ipairs({ "*", ".*" }) do
+    -- 	local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+    -- 	if match ~= "" then
+    -- 		return path.join(path.dirname(match), "bin", "python")
+    -- 	end
+    -- end
+    -- Fallback to system Python.
+    return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
 end
 
 return M
