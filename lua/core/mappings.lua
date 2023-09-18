813bd44543baa6d99411ac6b5b38a742fb31ffc6
Shide Foo <shide.foo@gojek.com>
2023-09-18 10:54:21 +0800

    Update git status mapping, lazy-lock.json

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,17 +1,5 @@
 local wk = require("which-key")
 local func = require("core.functions")
--- wk.register({
--- 	-- coc mappings
--- 	j = {
--- 		name = "coc", -- optional group name
--- 		d = { "<Plug>(coc-definition)", "Jump to definition" },
--- 		t = { "<Plug>(coc-type-definition)", "Jump to type" },
--- 		r = { "<Plug>(coc-references)", "Jump to references" },
--- 		n = { "<Plug>(coc-diagnostic-next-error)", "Jump to next error" },
--- 		p = { "<Plug>(coc-diagnostic-prev-error)", "Jump to previous error" },
--- 	},
--- }, { prefix = "<leader>", noremap = false })
-
 wk.register({
 	-- nvim-tree key-bindings
 	n = {
@@ -31,6 +19,10 @@ wk.register({
 		b = { "<cmd>Telescope file_browser<cr>", "File Browser" },
 		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
 	},
+    g = {
+        name = "git",
+        g = {"<cmd>vertical G<cr>", "Git status"}
+    }
 }, { prefix = "<leader>", silent = true })
 
 -- wk.register({
@@ -61,23 +53,6 @@ vim.keymap.set("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<cr>", { silent 
 vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns preview_hunk_inline<cr>", { silent = true })
 vim.keymap.set("n", "<leader>hx", "<cmd>Gitsigns reset_hunk<cr>", { silent = true })
 
--- coc-snippets
--- vim.cmd([[
--- let g:coc_snippet_next = '<C-l>'
--- let g:coc_snippet_prev = '<C-h>'
--- ]])
-
--- vim.cmd([[
---   nnoremap <silent> K :call ShowDocumentation()<CR>
---   " Show hover when provider exists, fallback to vim's builtin behavior.
---   function! ShowDocumentation()
---     if CocAction('hasProvider', 'hover')
---       call CocActionAsync('definitionHover')
---     else
---       call feedkeys('K', 'in')
---     endif
---   endfunction
--- ]])
 
 -- https://github.com/nvim-telescope/telescope.nvim/issues/814#issuecomment-1238510694
 vim.cmd([[
