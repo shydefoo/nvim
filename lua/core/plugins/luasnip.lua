d563123e52641a7ee90d5cc7402736d00d8f7e28
shydefoo <shidefoo@gmail.com>
2024-06-03 22:14:49 +0800

    Update lazynvim version, add copilot chat

--- lua/core/plugins/luasnip.lua
+++ lua/core/plugins/luasnip.lua
@@ -1,6 +1,6 @@
 local M = {
-	{ "L3MON4D3/LuaSnip", event = "VeryLazy" },
-	{ "rafamadriz/friendly-snippets", event = "VeryLazy"},
+	-- { "L3MON4D3/LuaSnip", event = "VeryLazy" },
+	-- { "rafamadriz/friendly-snippets", event = "VeryLazy"},
 }
 
 return M
